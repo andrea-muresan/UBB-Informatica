329017282124b82640a1de96b9539cc6b601d6a0
andrea-muresan <andreasimina.muresan@gmail.com>
2024-03-23 18:30:51 +0200

    AstronomicalEventsImages

---
+++ Anul_2/Semestrul_2/Sisteme de gestiune a bazelor de date/Project1/Project1/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPhotographer = new System.Windows.Forms.TextBox();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.textBoxCountry = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.dateTimePickerDate = new System.Windows.Forms.DateTimePicker();
+            this.dataGridViewEvents = new System.Windows.Forms.DataGridView();
+            this.dataGridViewImages = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewImages)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Goudy Old Style", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 560);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Photographer:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(290, 562);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(432, 562);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "URL:";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(657, 562);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "City:";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(825, 562);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Country:";
+            // 
+            // textBoxPhotographer
+            // 
+            this.textBoxPhotographer.Font = new System.Drawing.Font("Goudy Old Style", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPhotographer.Location = new System.Drawing.Point(25, 587);
+            this.textBoxPhotographer.Name = "textBoxPhotographer";
+            this.textBoxPhotographer.Size = new System.Drawing.Size(235, 32);
+            this.textBoxPhotographer.TabIndex = 5;
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Font = new System.Drawing.Font("Goudy Old Style", 10F);
+            this.textBoxURL.Location = new System.Drawing.Point(432, 587);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(196, 32);
+            this.textBoxURL.TabIndex = 7;
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Font = new System.Drawing.Font("Goudy Old Style", 10F);
+            this.textBoxCity.Location = new System.Drawing.Point(657, 587);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(139, 32);
+            this.textBoxCity.TabIndex = 8;
+            // 
+            // textBoxCountry
+            // 
+            this.textBoxCountry.Font = new System.Drawing.Font("Goudy Old Style", 10F);
+            this.textBoxCountry.Location = new System.Drawing.Point(825, 587);
+            this.textBoxCountry.Name = "textBoxCountry";
+            this.textBoxCountry.Size = new System.Drawing.Size(139, 32);
+            this.textBoxCountry.TabIndex = 9;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Goudy Old Style", 10F);
+            this.buttonAdd.Location = new System.Drawing.Point(312, 635);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(109, 42);
+            this.buttonAdd.TabIndex = 10;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Font = new System.Drawing.Font("Goudy Old Style", 10F);
+            this.buttonUpdate.Location = new System.Drawing.Point(444, 635);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(109, 42);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Goudy Old Style", 10F);
+            this.buttonDelete.Location = new System.Drawing.Point(576, 635);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(109, 42);
+            this.buttonDelete.TabIndex = 12;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // dateTimePickerDate
+            // 
+            this.dateTimePickerDate.CalendarFont = new System.Drawing.Font("Goudy Old Style", 10F);
+            this.dateTimePickerDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerDate.Location = new System.Drawing.Point(290, 587);
+            this.dateTimePickerDate.Name = "dateTimePickerDate";
+            this.dateTimePickerDate.Size = new System.Drawing.Size(119, 26);
+            this.dateTimePickerDate.TabIndex = 13;
+            // 
+            // dataGridViewEvents
+            // 
+            this.dataGridViewEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEvents.Location = new System.Drawing.Point(25, 48);
+            this.dataGridViewEvents.Name = "dataGridViewEvents";
+            this.dataGridViewEvents.RowTemplate.Height = 28;
+            this.dataGridViewEvents.Size = new System.Drawing.Size(939, 223);
+            this.dataGridViewEvents.TabIndex = 14;
+            // 
+            // dataGridViewImages
+            // 
+            this.dataGridViewImages.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewImages.Location = new System.Drawing.Point(25, 315);
+            this.dataGridViewImages.Name = "dataGridViewImages";
+            this.dataGridViewImages.RowTemplate.Height = 28;
+            this.dataGridViewImages.Size = new System.Drawing.Size(939, 223);
+            this.dataGridViewImages.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(25, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 23);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Events:";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(25, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 22);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Images:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 706);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridViewImages);
+            this.Controls.Add(this.dataGridViewEvents);
+            this.Controls.Add(this.dateTimePickerDate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxCountry);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.textBoxPhotographer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewImages)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+
+        private System.Windows.Forms.DataGridView dataGridViewImages;
+
+        private System.Windows.Forms.DataGridView dataGridViewEvents;
+
+        private System.Windows.Forms.DateTimePicker dateTimePickerDate;
+
+        private System.Windows.Forms.Button buttonDelete;
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonUpdate;
+
+        private System.Windows.Forms.TextBox textBoxPhotographer;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.TextBox textBoxCountry;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ Anul_2/Semestrul_2/Sisteme de gestiune a bazelor de date/Project1/Project1/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Server=LAPTOP-7V5C7GV\SQLEXPRESS; Database=AstronomicalEvents2; 
+            Integrated Security=true; TrustServerCertificate=true;";
+        DataSet ds = new DataSet();
+        SqlDataAdapter parentAdapter;
+        SqlDataAdapter childAdapter;
+        BindingSource bsParent;
+        BindingSource bsChild;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    parentAdapter = new SqlDataAdapter("SELECT * FROM event", conn);
+                    childAdapter = new SqlDataAdapter("SELECT * FROM image", conn);
+                    parentAdapter.Fill(ds, "event");
+                    childAdapter.Fill(ds, "image");
+                    bsParent = new BindingSource();
+                    bsParent.DataSource = ds.Tables["event"];
+                    dataGridViewEvents.DataSource = bsParent;
+                    DataColumn parentColumn = ds.Tables["event"].Columns["id"];
+                    DataColumn childColumn = ds.Tables["image"].Columns["event_id"];
+
+                    DataRelation relation = new DataRelation("FK_image_event", parentColumn, childColumn);
+                    ds.Relations.Add(relation);
+                    bsChild = new BindingSource();
+                    bsChild.DataSource = bsParent;
+                    bsChild.DataMember = "FK_image_event";
+                    dataGridViewImages.DataSource = bsChild;
+
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewEvents.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("Select an event!");
+                return;
+            } else if (dataGridViewEvents.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("Select only an event!");
+                return;
+            }
+            
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand insertCommand = new SqlCommand(
+                        "INSERT INTO image (event_id, photographer, date_taken, image_url, city, country) " +
+                        "VALUES (@event_val, @photographer_val, @date_val, @url_val, @city_val, @country_val);", conn);
+
+                    int ev_id = Convert.ToInt32(dataGridViewEvents.Rows[dataGridViewEvents.CurrentCell.RowIndex].Cells[0].Value);
+                    insertCommand.Parameters.AddWithValue("@event_val", ev_id);
+                    insertCommand.Parameters.AddWithValue("@photographer_val", textBoxPhotographer.Text);
+                    insertCommand.Parameters.AddWithValue("@date_val", dateTimePickerDate.Value);
+                    insertCommand.Parameters.AddWithValue("@url_val", textBoxURL.Text);
+                    insertCommand.Parameters.AddWithValue("@city_val", textBoxCity.Text);
+                    insertCommand.Parameters.AddWithValue("@country_val", textBoxCountry.Text);
+
+                    int insertRowCount = insertCommand.ExecuteNonQuery();
+                    
+                    childAdapter.SelectCommand.Connection = conn;
+                    ds.Tables["image"].Clear();
+                    childAdapter.Fill(ds, "image");
+
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewImages.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("Select an image!");
+                return;
+            } else if (dataGridViewImages.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("Select only an image!");
+                return;
+            }
+            
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand deleteCommand = new SqlCommand("DELETE FROM image WHERE id=@id;", conn);
+                    
+                    int im_id = Convert.ToInt32(dataGridViewImages.Rows[dataGridViewImages.CurrentCell.RowIndex].Cells[0].Value);
+                    Console.WriteLine(im_id);
+                    deleteCommand.Parameters.AddWithValue("@id", im_id);
+
+                    int deleteRowCount = deleteCommand.ExecuteNonQuery();
+                    
+                    childAdapter.SelectCommand.Connection = conn;
+                    ds.Tables["image"].Clear();
+                    childAdapter.Fill(ds, "image");
+                    
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+            
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewImages.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("Select an image!");
+                return;
+            } else if (dataGridViewImages.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("Select only an image!");
+                return;
+            }
+            
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand updateCommand = new SqlCommand("UPDATE image " + 
+                                                              "SET photographer=@photographer_val, date_taken=@date_val, image_url=@url_val, city=@city_val, country=@country_val " + 
+                                                              "WHERE id=@id;", conn);
+                    
+                    int im_id = Convert.ToInt32(dataGridViewImages.Rows[dataGridViewImages.CurrentCell.RowIndex].Cells[0].Value);
+                    Console.WriteLine(im_id);
+                    updateCommand.Parameters.AddWithValue("@id", im_id);
+                    updateCommand.Parameters.AddWithValue("@photographer_val", textBoxPhotographer.Text);
+                    updateCommand.Parameters.AddWithValue("@date_val", dateTimePickerDate.Value);
+                    updateCommand.Parameters.AddWithValue("@url_val", textBoxURL.Text);
+                    updateCommand.Parameters.AddWithValue("@city_val", textBoxCity.Text);
+                    updateCommand.Parameters.AddWithValue("@country_val", textBoxCountry.Text);
+                    
+                    int deleteRowCount = updateCommand.ExecuteNonQuery();
+                    
+                    childAdapter.SelectCommand.Connection = conn;
+                    ds.Tables["image"].Clear();
+                    childAdapter.Fill(ds, "image");
+                    
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+
+        private void ClearInputFields()
+        {
+            textBoxPhotographer.Clear();
+            dateTimePickerDate.ResetText();
+            textBoxURL.Clear();
+            textBoxCity.Clear();
+            textBoxCountry.Clear();
+        }
+    }
+}
